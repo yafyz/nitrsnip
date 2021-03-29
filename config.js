b67df47293d7bd1a7a44f7bd0d3707a02f1a067a
fyz <36511274+yafyz@users.noreply.github.com>
2021-03-29 20:59:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ if (fs.existsSync("files")) {
         "d_token": process.env.d_token,
         "d_gateway": "wss://gateway.discord.gg/?encoding=etf&v=8&compress=zlib-stream",
         "d_webhook": process.env.d_webhook,
-        "d_err_webhook": process.env.d_webhook,
+        "d_err_webhook": process.env.d_err_webhook,
         "read_messages_on_redeem_account": process.env.read_messages_on_redeem_account.toLowerCase() == "true",
         "use_multiple_tokens": process.env.use_multiple_tokens.toLowerCase() == "true",
         "tokens": process.env.tokens,

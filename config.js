6fe43003409451ba2eb3ff4bc6807a150da89e27
p00rprogrammer <105941365+p00rprogrammer@users.noreply.github.com>
2022-05-20 10:26:20 -0700

    Update config.js
    able to access secrets e.g tokens, main token, sniping webhook, error webhook. sensitive stuff goes to secrets always.

--- config.js
+++ config.js
@@ -1,18 +1,21 @@
-const fs = require("fs");
-if (fs.existsSync("files")) {
-    module.exports = JSON.parse(fs.readFileSync("files/config.json"));
-} else {
+
+const mainToken = process.env['mainToken']
+const AltToken = process.env['AltToken']
+
+const d_webhook = process.env['d_webhook']
+const d_err_webhook = process.env['d_err_webhook']
+
+
     module.exports = {
-        "d_token": process.env.d_token,
+        "d_token": process.env.mainToken,
         "d_gateway": "wss://gateway.discord.gg/?encoding=etf&v=9&compress=zlib-stream",
         "d_webhook": process.env.d_webhook,
         "d_err_webhook": process.env.d_err_webhook || process.env.d_webhook,
-        "read_messages_on_redeem_account": process.env.read_messages_on_redeem_account.toLowerCase() == "true",
+   "read_messages_on_redeem_account": true,
         "debug_webhook": process.env.debug_webhook,
-        "use_multiple_tokens": process.env.use_multiple_tokens.toLowerCase() == "true",
-        "tokens": process.env.tokens,
-        "show_messages": process.env.show_messages.toLowerCase() == "true",
+    "use_multiple_tokens": true,
+        "tokens": process.env.AltToken,
+        "show_messages": false,
         "cache_codes": true,
-        "improve_latency": typeof process.env.improve_latency != "undefined"
+        "improve_latency": true
     }
-}
